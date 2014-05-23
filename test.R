bd6259a84dd5ace2865b25c3004462ce1eb20e39
robmoore <rob.moore@gmail.com>
2014-05-23 07:44:50 -0500

    Adding some more examples for testing.

--- test.R
+++ test.R
@@ -8,7 +8,7 @@ ide <- mat %*% inv
 
 # Generate a random 2 x 2 matrix of integers from 1 to 10
 di <- 2
-mat <- matrix(sample(1:10, di * di), di, di)
+mat <- matrix(sample(1:10, di ^ 2), di, di)
 # Invert matrix
 inv <- solve(mat)
 # General identity matrix
@@ -24,3 +24,21 @@ cs <- cacheSolve(cm)
 is.null(cm$gets())
 
 identical(inv, cacheSolve(cm))
+
+# Test using large matrix so caching is evident
+lm <- matrix(runif(di ^ 2), di)
+cm <- makeCacheMatrix(lm)
+# Should take some time
+system.time(cs <- cacheSolve(cm))
+# Should be much faster
+system.time(cs <- cacheSolve(cm))
+identical(solve(lm), cs)
+
+# Should produce proper identity matrix
+round(lm %*% cs, 4)
+
+# Confirm
+
+system.time(t1 <- cacheSolve(test)) 
+
+system.time(t1 <- cacheSolve(test)) 
